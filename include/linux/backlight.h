8e79688f9b31a6e88cb614c170671b3575e9702e
x0x00044 <zlewchan@icloud.com>
2024-03-11 23:54:09 +0100

    include: Import remaining Nokia header changes
    Change-Id: I550030e451d9a09db61ac8177b246b48bf8f85a7

--- include/linux/backlight.h
+++ include/linux/backlight.h
@@ -69,6 +69,8 @@ struct backlight_ops {
 struct backlight_properties {
 	/* Current User requested brightness (0 - max_brightness) */
 	int brightness;
+	// Nokia addition
+	int boost_mode;
 	/* Maximal value for brightness (read-only) */
 	int max_brightness;
 	/* Current FB Power mode (0: full on, 1..3: power saving
